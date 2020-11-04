aa10b81e31136635f3f1ca5cdc5529951bc96a15
nanandt <rizkyfatih007@gmail.com>
2020-11-05 01:05:47 +0700

    membuat API transaksi

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('products', 'API\ProductController@all');
 Route::post('checkout', 'API\CheckoutController@checkout');
+Route::get('transactions/{id}', 'API\TransactionController@get');
